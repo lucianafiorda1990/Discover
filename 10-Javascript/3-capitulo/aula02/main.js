1b6e9e9d3100cc24938b580a40e6409fa27ddd9d
Luuuh <luzinha_fiorda@hotmail.com>
2022-01-04 16:35:57 -0300

    Aula 02 - String

---
+++ 10-Javascript/3-capitulo/aula02/main.js
@@ -0,0 +1,16 @@
+console.log('Luciana')
+console.log("Luciana")
+console.log(`Luciana`) //template literals ou template string
+
+//exemplo de texto multilinhas
+
+console.log(`
+Aqui
+tem
+
+uma quebra de linha
+`)
+
+console.log(`Don't look up "baby"`)
+
+console.log(`1 + 1 = ${1 + 1}`)
