d2a75a7f679d1495803c73e0e006be73efde40d4
Luuuh <luzinha_fiorda@hotmail.com>
2022-01-14 12:02:00 -0300

    Aula 02 - require/ modulos

---
+++ 16-NodeJS/3-capitulo/aula02/require.js
@@ -0,0 +1,4 @@
+const path = require('path')
+
+console.log(`Basename do __filaname: `, path.basename("__filename)"))
+console.log(`Basename de um arquivo do pc: `, path.basename("C:/Projetos/Discover/16-NodeJS/3-capitulo/aula02"))
