2e58c4228100c338483f3cb463129e0b914ed816
Luuuh <luzinha_fiorda@hotmail.com>
2022-01-14 11:37:53 -0300

    Aula 01 - Primeiro projeto e Globals

---
+++ 16-NodeJS/3-capitulo/console.js
@@ -0,0 +1,9 @@
+global.console.log('Hello')
+
+//globals
+
+//Diretorio atual, onde está o arquivo em que estamos executando
+console.log(__dirname)
+
+//Caminho completo + nome do arquivo
+console.log(__filename)
