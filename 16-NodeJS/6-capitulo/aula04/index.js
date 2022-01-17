432cf36ff7458b0e868e2ccb313381965d1021c4
Luuuh <luzinha_fiorda@hotmail.com>
2022-01-17 10:58:43 -0300

    Aula 04 - setInterval()

---
+++ 16-NodeJS/6-capitulo/aula04/index.js
@@ -0,0 +1,8 @@
+// setInerval -> irá rodar uma função N vezes depois de intervalos de X milissegundos
+
+const timeOut = 1000
+const cheking = () => console.log('checking!')
+
+setInterval(cheking, timeOut)
+
+//é preciso dar ctrl + c para parar a função no console
