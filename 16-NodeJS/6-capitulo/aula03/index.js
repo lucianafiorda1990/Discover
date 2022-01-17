c905bfb0141f18826bf2cec70a76a7c41d883a7d
Luuuh <luzinha_fiorda@hotmail.com>
2022-01-17 10:49:14 -0300

    Aula 03 - clearTimeout()

---
+++ 16-NodeJS/6-capitulo/aula03/index.js
@@ -0,0 +1,7 @@
+// clearTimeout - cancelar uma função de setTimeout
+
+const timeOut = 3000
+const finished = () => console.log('Done!')
+
+let timer = setTimeout(finished, timeOut)
+clearTimeout(timer)
