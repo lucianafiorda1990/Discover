1b5597fcb36d2771f194eb70321889662c217618
Luuuh <luzinha_fiorda@hotmail.com>
2022-01-19 16:40:10 -0300

    Aula 10 e 11 - Queue (fila)

---
+++ 19-Estrutura-de-dados/aula10e11/main.js
@@ -0,0 +1,45 @@
+// ### QUEUE ###
+
+/*
+Queue = fila
+-> linear
+-> o primeiro a entrar na fila, é o primeiro a sair
+-> FIFO - "First In, First Out"
+-> Enqueue = entrando na fila
+-> Dequeue = saindo da fila
+*/
+
+//enqueue() -> adiciona um elemento ao final da fila
+//dequeue() -> remove o primeiro elemento a entrar na fila
+//size() -> para mostrar o tamanho da fila
+//front() -> para pegar o primeiro elemento da fila
+
+//Passo 1 - Modelagem
+class Queue {
+    constructor() {
+        this.data = []
+    }
+
+    enqueue(item) {
+        this.data.push(item)
+        console.log(`${item} chegou na fila!`)
+    }
+
+    dequeue() {
+        const item = this.data.shift()
+        console.log(`${item} saiu da fila!`)
+    }
+}
+
+
+//Passo 2 - Utilização
+const fila = new Queue()
+
+//adicionando dados
+fila.enqueue('Mariana')
+fila.enqueue('Jorge')
+fila.enqueue('Paola')
+
+fila.dequeue()
+fila.dequeue()
+fila.dequeue()
