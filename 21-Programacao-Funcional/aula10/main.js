f8f2393efedf7b1fff0eec3c3d2a07ff8fbcf8ab
Luuuh <luzinha_fiorda@hotmail.com>
2022-01-20 15:57:34 -0300

    Aula 10 - Funçoes Independentes

---
+++ 21-Programacao-Funcional/aula10/main.js
@@ -0,0 +1,25 @@
+// ### FUNÇOES INDEPENDENTES ###
+
+/*
+-> Deverá ter ao menos 1 argumento
+-> Deverá retornar algo
+-> Nada que acontecer lá dentro afeta o mundo externo
+    - dados imutáveis
+    - não guarda estado
+-> Não faremos uso de loops (for/while), mas se houver necessidade de tal, usaremos recursividade
+*/
+
+const random = (number, Math) => Math.floor(Math.random() * number)
+//como usar esse random?
+
+//recursive
+//finding the fatorial of a number
+
+const factorial = x => {
+    if (x === 0) {
+        return 1;
+    }
+    return x * factorial(x - 1)
+}
+
+console.log(factorial(6)) //720
