74567b76ce059faf3ba23e27a7f91cda73649f94
Luuuh <luzinha_fiorda@hotmail.com>
2022-01-20 09:51:41 -0300

    Aula 14 - Polimorfismo

---
+++ 20-POO/aula14/main.js
@@ -0,0 +1,48 @@
+// ### POLIMORFISMO ###
+
+/* 
+-> Significa muitas formas
+
+-> Quando um objeto estende de outro (herança), alvez haja a necessidade de reescrever uma ou mais características (atributos e métodos) nesse novo objeto.
+*/
+
+class Atleta {
+    peso;
+    categoria;
+
+    constructor(peso) {
+        this.peso = peso
+    }
+
+    definirCategoria() {
+        if (this.peso <= 50) {
+            this.categoria = 'infantil'
+        } else if (this.peso <= 65) {
+            this.categoria = 'juvenil'
+        } else {
+            this.categoria = 'adulto'
+        }
+    }
+}
+
+class Lutador extends Atleta {
+    constructor(peso) {
+        super(peso)
+    }
+
+    definirCategoria() {
+        if (this.peso <= 54) {
+            this.categoria = 'pluma'
+        } else if (this.peso <= 60) {
+            this.categoria = 'leve'
+        } else if (this.peso <= 75) {
+            this.categoria = 'meio-leve'
+        } else {
+            this.categoria = 'pesado'
+        }
+        return this.categoria
+    }
+}
+
+let aldo = new Lutador(80)
+console.log(aldo.definirCategoria())
